34b1b68fc15bc489897e4b405344288a88207bd6
rails course <camaroa@hotmail.com>
2017-02-03 08:09:41 +0000

    bootstrap instaled

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   #rails give us getter / setters
-  validates :title, presence: true length: {minimum: 3, maximum 50}
-  validates :description, presence: true length: {minnimum: 10, maximum 300}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
 end
