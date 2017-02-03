df6b84a927a8c0853031335dd738755b9237a9f0
rails course <camaroa@hotmail.com>
2017-02-03 07:16:46 +0000

    ready for twitter boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
+  root 'pagse#home'
+  get 'about', to: 'pages#about'
   resources :articles
 
 #root 'welcome#home'
 #get 'welcome/home', to: 'welcome#home'
 #get 'about', to: 'welcome#about'
-get 'pages/home', to: 'pages#home'
-get 'pages/about', to: 'pages#about'
+#get 'pages/home', to: 'pages#home'
+#get 'pages/about', to: 'pages#about'
 
 end
