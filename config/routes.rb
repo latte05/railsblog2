42f673294f676bc20a17c473b34e3c736e19ec13
rails course <camaroa@hotmail.com>
2017-02-10 08:12:36 +0000

    sign-up form from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 #root 'welcome#home'
 #get 'welcome/home', to: 'welcome#home'
 #get 'about', to: 'welcome#about'
