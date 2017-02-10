3fa8f6d6df5c2261603eaa162025f57847f60489
rails course <camaroa@hotmail.com>
2017-02-10 21:34:26 +0000

    added user edit and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,45 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to the iced latte Blog #{@user.username} "
-      redirect_to articles_path
-    else
-      render 'new'
+
+    def index
+        @users = User.all
     end
-  end
 
-private
-  def user_params
-      params.require(:user).permit(:username, :email, :password)
-  end
+    def new
+      @user = User.new
+    end
 
-end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the iced latte Blog #{@user.username} "
+        redirect_to articles_path
+      else
+        render 'new'
+      end
+    end
+
+   def show
+     @user = User.find(params[:id])
+   end
+
+   def edit
+     @user = User.find(params[:id])
+   end
+
+   def update
+     @user = User.find(params[:id])
+     if @user.update(user_params)
+       flash[:success] = "Your account was updated succesffuly"
+        redirect_to articles_path
+     else
+        render "edit"
+     end
+   end
+
+  private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+  end
