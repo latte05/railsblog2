c2a05303265e6b74b978d5a2105750c5e567b870
rails course <camaroa@hotmail.com>
2017-02-11 06:48:14 +0000

    added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destory'
+
 #root 'welcome#home'
 #get 'welcome/home', to: 'welcome#home'
 #get 'about', to: 'welcome#about'
