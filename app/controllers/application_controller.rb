cde4edc883c489b45c882ad5b3d572ddda3a5576
rails course <camaroa@hotmail.com>
2017-02-12 05:45:20 +0000

    complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # avaialble to all controllers
+
+  #avaialble to views!
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # dont' hit DB everytime. if current_user return user the user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # true if there is current user, false if there isn't current user
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must logged in to performa that action"
+      redirect_to root_path
+    end
+  end
+
+
 end
